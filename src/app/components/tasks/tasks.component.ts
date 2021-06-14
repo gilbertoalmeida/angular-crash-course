75c3a497d424a315061fd6ba04f7aff69775837e
gilberto almeida <>
2021-06-14 23:31:52 +0200

    created a service to bring the tasks as an observable

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from "../../mock-tasks"
+import { TaskService } from "../../services/task.service"
 import { Task } from "../../Task"
 
 @Component({
@@ -8,11 +8,14 @@ import { Task } from "../../Task"
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS
+  tasks: Task[] = []
 
-  constructor() { }
+  //bringing the service as a provider
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    //fires of on init, so that's when we want to use the service to get the tasks.
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks) //since it is an observable, we subscribe and get something from it, it is kinda like a promisse
   }
 
 }
