d3bb3f4125182c2720e133e8a3fc925cf1d5588f
gilberto almeida <>
2021-06-14 23:51:58 +0200

    Added a local server using json-server, to do CRUD operations through a server in a local database file: db.json

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from "rxjs"
-import { TASKS } from "../mock-tasks"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { Observable, /* of */ } from "rxjs"
+// import { TASKS } from "../mock-tasks"
 import { Task } from "../Task"
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS) //need to use of because it is an observable
-    return tasks
+    // this is importing the tasks as an observale from the local mock file
+    // const tasks = of(TASKS) //need to use of because it is an observable
+    // return tasks
+
+    //this is importing the tasks as an observable from our local server from json-server. Database is db.json at the root. It's a mock server, it will do the CRUD operations in this local file database
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
