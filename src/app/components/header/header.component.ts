f847cfaf3c9d935a16b2460133d276b6efc85683
gilberto almeida <almeidanetogilberto@gmail.com>
2021-07-14 22:04:24 +0200

    Created a uiService that holds the state of showing the form or not. And I toggle this with the Add button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask!: boolean
+  subscription: Subscription
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    //I could have made the local showAddTask change in the local toggleAddTask, all inside this class alone
+    //However, I want the same variable to influence the button AND the form itself.
+    //That's why we made it all in a Service and we are subscribing to the variable
+    //everytime this.toggleAddTask fires, the toggleAddTask of the uiService will fire
+    //toggle the variable in the service and send it over a subject
+    //which we are subscribing here, can catch as value and make the local showAddTask equal to it.s
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    alert("add a task")
+    this.uiService.toggleAddTask()
   }
 
 }
