be3f1a6c1f168bfd4058692b2e494723502e2878
gilberto almeida <almeidanetogilberto@gmail.com>
2021-07-14 22:48:58 +0200

    Added a footer component and cnditioned the Add button to the / route where the tasks are

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask!: boolean
   subscription: Subscription
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     //I could have made the local showAddTask change in the local toggleAddTask, all inside this class alone
     //However, I want the same variable to influence the button AND the form itself.
     //That's why we made it all in a Service and we are subscribing to the variable
@@ -29,4 +30,7 @@ export class HeaderComponent implements OnInit {
     this.uiService.toggleAddTask()
   }
 
+  hasRoute(route: string) {
+    return this.router.url === route
+  }
 }
